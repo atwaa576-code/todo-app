9400fa14d445f33c7c8a0792811f45c902ea165b
Ahmed <atwa576@gmail.com>
2026-09-15 23:42:19 +0300

    Add local storage

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,8 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function TodoForm() {
   const [input, setInput] = useState("");
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem("tasks");
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -49,78 +57,78 @@ function TodoForm() {
 
   return (
     <>
-    <h1>To Do List APP</h1>
-    <form onSubmit={handleSubmit}>
-      <div className="input-container">
-        <input
-          className="input-field"
-          type="text"
-          placeholder="Enter a task"
-          onChange={handleChange}
-          value={input}
-        />
+      <h1>To Do List APP</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <input
+            className="input-field"
+            type="text"
+            placeholder="Enter a task"
+            onChange={handleChange}
+            value={input}
+          />
 
-        <button type="button" className="btn add-btn" onClick={addTask}>
-          Add
+          <button type="button" className="btn add-btn" onClick={addTask}>
+            Add
+          </button>
+        </div>
+
+        <button
+          type="button"
+          className="btn clear-btn"
+          onClick={() => setTasks([])}
+        >
+          Clear All
         </button>
-      </div>
+        <div>
+          {tasks.map((task) => (
+            <div
+              className={`task ${task.completed ? "completed" : ""}`}
+              key={task.id}
+            >
+              <p>
+                <span
+                  style={{
+                    textDecoration: task.completed ? "line-through" : "none",
+                  }}
+                >
+                  {task.text}
+                </span>
 
-      <button
-        type="button"
-        className="btn clear-btn"
-        onClick={() => setTasks([])}
-      >
-        Clear All
-      </button>
-      <div>
-        {tasks.map((task) => (
-          <div
-            className={`task ${task.completed ? "completed" : ""}`}
-            key={task.id}
-          >
-            <p>
-              <span
-                style={{
-                  textDecoration: task.completed ? "line-through" : "none",
+                {task.completed ? "✅ " : "❌ "}
+              </p>
+              <button
+                className="btn delete-btn"
+                type="button"
+                onClick={() => deleteTask(task.id)}
+              >
+                Delete
+              </button>
+
+              <button
+                type="button"
+                className="btn edit-btn"
+                onClick={() => {
+                  editTask(task.id);
                 }}
               >
-                {task.text}
-              </span>
+                Edit
+              </button>
 
-              {task.completed ? "✅ " : "❌ "}
-            </p>
-            <button
-              className="btn delete-btn"
-              type="button"
-              onClick={() => deleteTask(task.id)}
-            >
-              Delete
-            </button>
+              <button
+                className="btn complete-btn"
+                type="button"
+                onClick={() => completedTask(task.id)}
+              >
+                {task.completed ? "Undo" : "complete"}
+              </button>
+            </div>
+          ))}
+        </div>
 
-            <button
-              type="button"
-              className="btn edit-btn"
-              onClick={() => {
-                editTask(task.id);
-              }}
-            >
-              Edit
-            </button>
-
-            <button
-              className="btn complete-btn"
-              type="button"
-              onClick={() => completedTask(task.id)}
-            >
-              {task.completed ? "Undo" : "complete"}
-            </button>
-          </div>
-        ))}
-      </div>
-
-      <p>Remaining :{tasks.filter((task) => !task.completed).length} </p>
-      <p>Completed :{tasks.filter((task) => task.completed).length} </p>
-    </form>
+        <p>Remaining :{tasks.filter((task) => !task.completed).length} </p>
+        <p>Completed :{tasks.filter((task) => task.completed).length} </p>
+      </form>
     </>
   );
 }
